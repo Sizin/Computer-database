d918a34405358e652a65b06aae8dbc7c5871ab3c
sizin <sacar@excilys.com>
2018-09-24 18:29:43 +0200

    [FIX] Removing old arboresence

--- src/com/excilys/cdb/exceptions/DateFormatException.java
+++
@@ -1,9 +0,0 @@
-package com.excilys.cdb.exceptions;
-
-public class DateFormatException extends Exception {
-
-	public DateFormatException() {
-		System.out.println("Le format de la date n'est pas bon, recommencez");
-	}
-
-}
